f4fcf1e3918338ebf9fe2c4f9ba120b3e6843d2b
MatNovak <matnovak@outlook.com>
2025-08-17 10:18:21 +0200

    add file writter

---
+++ WebSiteDownloader/Helpers/FileWriter/FileWriter.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace WebSiteDownloader.Helpers.FileWriter;
+
+public class FileWriter : IFileWriter
+{
+    public void WriteContent(string filePath, string content)
+    {
+        if (string.IsNullOrWhiteSpace(filePath))
+        {
+            throw new ArgumentException("File path cannot be null or empty.", nameof(filePath));
+        }
+        if (string.IsNullOrWhiteSpace(content))
+        {
+            throw new ArgumentException("Content cannot be null or empty.", nameof(content));
+        }
+        if (File.Exists(filePath))
+        {
+            filePath = Path.Combine(Path.GetDirectoryName(filePath)??string.Empty,
+                $"{Path.GetFileNameWithoutExtension(filePath)}_{Stopwatch.GetTimestamp()}{Path.GetExtension(filePath)}");
+        }
+        File.WriteAllText(filePath, content);
+    }
+}
---
+++ WebSiteDownloader/Helpers/FileWriter/IFileWriter.cs
@@ -0,0 +1,11 @@
+﻿namespace WebSiteDownloader.Helpers.FileWriter;
+
+public interface IFileWriter
+{
+    /// <summary>
+    /// Savings the website contents to the specified output folder.
+    /// </summary>
+    /// <param name="filePath">The path to the file where the content will be saved.</param>
+    /// <param name="content">The content to be written to the file.</param>
+    void WriteContent(string filePath, string content);
+}
